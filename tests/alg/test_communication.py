b7bbd2fdffb1c223fa4b3dd5b85ff41aed1ea681
Gun <cfr2ak@gmail.com>
2021-11-24 02:56:50 +0900

    Changed file names

---
+++ tests/alg/test_communication.py
@@ -0,0 +1,42 @@
+import pytest
+from willard import alg
+from willard.type import qreg
+
+
+def test_spy_detector():
+    def spy(q):
+        from willard.type import qreg
+        q.h().measure()
+        q = qreg(1).bits('0')
+        q.x().h()
+        return q
+    assert(alg.detect_spy(spy))
+
+    def no_spy(q):
+        return q
+    assert(not alg.detect_spy(no_spy))
+
+
+def test_teleportation():
+    qr = qreg(3)
+    alice = qr.bits('1')
+    channel = qr.bits('0')
+    bob = qr.bits('0')
+
+    alice.teleport(bob, channel)
+    got = int(bob.measure())
+    want = 1
+
+    assert(got == want)
+
+
+def test_superdense_coding():
+    pass
+
+
+def test_qkd():
+    pass
+
+
+def test_ckd():
+    pass
--- tests/alg/test_search.py
+++ tests/alg/test_search.py
@@ -80,3 +80,32 @@ def test_bernstein_vazirani():
     got = alg.bv(x, y, s_101)
     want = '101'
     assert(got == want)
+
+
+def test_simon():
+    qr = qreg(4)
+    x = qr.bits('00')
+    y = qr.bits('00')
+
+    def period_11(x, y):
+        x[0].cx(y[0]).cx(y[1])
+        x[1].cx(y[0]).cx(y[1])
+    alg.simon(x, y, period_11)
+    got = x.measure()
+    want1 = '00'
+    want2 = '11'
+    assert(got == want1 or got == want2)
+
+
+def test_shor():
+    pass
+
+
+def test_grover():
+    # qr = qreg(3)
+    # q = qr.bits('000')
+    # q.grover(oracle1)
+
+    # qr.reset()
+    # q.grover(oracle2)
+    pass
