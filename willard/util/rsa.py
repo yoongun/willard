88be252bc6489bfe9383c45cfe222d359b7bae55
Gun <cfr2ak@gmail.com>
2021-11-25 02:35:45 +0900

    Refactored member methods of rsa.py

--- willard/util/rsa.py
+++ willard/util/rsa.py
@@ -7,8 +7,8 @@ class rsa:
             raise ValueError("p, q should be a prime number.")
         self.n = p * q
         self.phi = (p - 1) * (q - 1)
-        self.e = max(self.coprimes(self.phi))
-        self.d = self.modinv(self.e, self.phi)
+        self.e = max(self._coprimes(self.phi))
+        self.d = self._modinv(self.e, self.phi)
         self.public_key = (self.e, self.n)
         self.private_key = (self.d, self.n)
 
@@ -23,43 +23,43 @@ class rsa:
                     return False
         return True
 
-    def coprimes(self, a):
+    def _coprimes(self, a):
         l = []
         for x in range(2, a):
-            if self.gcd(a, x) == 1 and self.modinv(x, self.phi) is not None:
+            if self._gcd(a, x) == 1 and self._modinv(x, self.phi) is not None:
                 l.append(x)
         for x in l:
-            if x == self.modinv(x, self.phi):
+            if x == self._modinv(x, self.phi):
                 l.remove(x)
         return l
 
-    def gcd(self, a, b):
+    def _gcd(self, a, b):
         while b != 0:
             c = a % b
             a = b
             b = c
         return a
 
-    def modinv(self, a, m):
-        for x in range(1, m):
-            if (a * x) % m == 1:
+    def _modinv(self, a, n):
+        for x in range(1, n):
+            if (a * x) % n == 1:
                 return x
         return None
 
-    def encrypt_block(self, m):
-        c = self.modinv(m ** self.e, self.n)
+    def _encrypt_block(self, m):
+        c = self._modinv(m ** self.e, self.n)
         if c is None:
             print('No modular multiplicative inverse for block ' + str(m) + '.')
         return c
 
-    def decrypt_block(self, c):
-        m = self.modinv(c ** self.d, self.n)
+    def _decrypt_block(self, c):
+        m = self._modinv(c ** self.d, self.n)
         if m is None:
             print('No modular multiplicative inverse for block ' + str(c) + '.')
         return m
 
     def encrypt(self, s):
-        return ''.join([chr(self.encrypt_block(ord(x))) for x in list(s)])
+        return ''.join([chr(self._encrypt_block(ord(x))) for x in list(s)])
 
     def decrypt(self, s):
-        return ''.join([chr(self.decrypt_block(ord(x))) for x in list(s)])
+        return ''.join([chr(self._decrypt_block(ord(x))) for x in list(s)])
