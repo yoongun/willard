9d6751d8e2ba28f07fd231db7af4eb9d68c8f9d6
Gun <cfr2ak@gmail.com>
2021-11-25 04:11:38 +0900

    Added test for oracle

---
+++ tests/type/test_oracle.py
@@ -0,0 +1,25 @@
+# import pytest
+# from willard.type import basis_oracle
+# # from willard.type import phase_oracle
+
+
+# @pytest.fixture()
+# def qr():
+#     from willard.type import qreg
+#     return qreg
+
+
+# def test_phase_oracle():
+#     pass
+
+
+# def test_basis_oracle(qr):
+#     def f(x: int) -> int:
+#         return x * 2
+
+#     basis_oracle(n, m, f)
+
+#     def f(x: int) -> int:
+#         a = 7
+#         N = 15
+#         return (a ** x) % N
